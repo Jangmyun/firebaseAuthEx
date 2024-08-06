2cd01283137204c24b41e7d5c709dcf385b252cd
Jangmyun <cndlsrb2739@gmail.com>
2024-08-06 09:31:27 +0900

    .

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,9 +1,38 @@
+import { Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
+
 const { Link } = require("react-router-dom");
 
 function Home(props) {
   return (
     <div id="homeContainer">
       <Link to="/login">로그인 페이지 이동</Link>
+      <Container>
+        <Row xs={1} fluid="sm">
+          <Col md={5}>
+            <FormGroup className="">
+              <Label for="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                placeholder="handonge@handong.ac.kr"
+                type="email"
+                onChange={(e) => {
+                  console.log(e.target.value);
+                }}
+              ></Input>
+              <Label for="password" className="">
+                password
+              </Label>
+              <Input
+                id="password"
+                name="password"
+                placeholder="password"
+                type="password"
+              ></Input>
+            </FormGroup>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
