2b10fc0705ffb9aa05f0a6d6d8c3b02987479fe4
Jangmyun <cndlsrb2739@gmail.com>
2024-08-12 11:25:39 +0900

    nodemailer 사용한 이메일 전송 성공 예제

--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
+require("dotenv").config();
+
+// nodemailer
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "cra2024.2048@gmail.com",
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+const mailOption = {
+  to: "cndlsrb2739@gmail.com",
+  subject: "Test Email",
+  html: `
+  <h1>테스트 메시지입니다.</h1>
+  <h5>해당 부분에 인증코드 들어갑니다</h5>
+  `,
+};
+
+async function SendmailTransport() {
+  await transporter.sendMail(mailOption);
+}
+
+SendmailTransport();
 
 // Session
 const session = require("express-session");
